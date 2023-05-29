1a76c4aa46ea99213618c2192329e67a2798f8a2
s28413-pj <117545944+28413-pj@users.noreply.github.com>
2023-05-29 19:18:14 +0200

    Create zad1.c

---
+++ s28413-MarekJenczyk/zjazd4/zad1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	int w=0; int l=0;
+	do{
+		l=0;
+		printf("Podaj liczbe: ");
+		scanf("%d",&l);
+		w+=li
+	}while(li != 0);
+	printf("Wynik:%d\n",w);
+	return w;
+}
